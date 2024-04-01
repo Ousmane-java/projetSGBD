9751785235277e6ce0b157a6f2b3bef16f58210c
aminata054 <tinaami054@gmail.com>
2024-04-01 21:14:53 +0000

    Ajout de la page d'accueil, de connexion et d'informations

--- angularProject/src/app/app.routes.ts
+++ angularProject/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent},
+    { path: 'connexion', component: LoginPageComponent},
+    { path: 'infos', component: AboutComponent},
+];
