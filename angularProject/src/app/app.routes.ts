2f707ac30e04c084cec418bbd160f803e9b9b4e7
aminata054 <tinaami054@gmail.com>
2024-04-04 11:09:01 +0000

    Mise a jour

--- angularProject/src/app/app.routes.ts
+++ angularProject/src/app/app.routes.ts
@@ -1,11 +1,46 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { DashboardComponent } from './etudiant/dashboard/dashboard.component';
+import { Component, NgModule } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { EnseignementComponent } from './etudiant/enseignement/enseignement.component';
+import { FeedbackComponent } from './etudiant/feedback/feedback.component';
+import { AccueilComponent } from './etudiant/accueil/accueil.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent},
-    { path: 'infos', component: AboutComponent},
-    { path: 'connexion', component: LoginPageComponent},
+
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+      },
+    {
+        
+        path: '', component: HeaderComponent,
+        children: [
+            { path: 'home', component: HomeComponent},
+            { path: 'infos', component: AboutComponent},
+            { path: 'login', component: LoginPageComponent},
+        ]
+    },
+
+    { path: 'dashboard', component: DashboardComponent, children: [
+
+        { path: 'accueil',  component: AccueilComponent,},
+        { path: 'enseignement',  component: EnseignementComponent},
+        { path: 'feedback',  component: FeedbackComponent},
+            
+    ]},
+
+    
 
 ];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutes { }
